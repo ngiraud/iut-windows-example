1a3a2e8b6d515815fb8d5053c89d6ec9d2056c05
Nicolas Giraud <nicolas.giraud01@gmail.com>
2023-10-14 14:35:04 +0200

    Adding pagination

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,14 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Post::factory()
+        ->for(User::where('email', 'contact@ngiraud.me')->first())
+        ->count(50)
+        ->create();
+
+        Post::factory()
+        ->for(User::where('email', 'john@example.com')->first())
+        ->count(100)
+        ->create();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,53 +20,59 @@
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="flex flex-col">
                     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                        <div class="overflow-hidden">
-                            <table class="min-w-full w-full text-left text-sm font-light">
-                            <thead
-                                class="border-b bg-white font-medium dark:border-neutral-500 dark:bg-neutral-600">
-                                <tr>
-                                <th scope="col" class="px-6 py-4">#</th>
-                                <th scope="col" class="px-6 py-4">Title</th>
-                                <th scope="col" class="px-6 py-4">Excerpt</th>
-                                <th scope="col" class="px-6 py-4"># words</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($posts as $post)
-                                <tr class="border-b {{ $loop->odd ? 'bg-white' : 'bg-neutral-100' }} dark:border-neutral-500 dark:bg-neutral-700">
-                                    <td class="whitespace-nowrap px-6 py-4 font-medium">
-                                        {{ $post->id }}
-                                    </td>
-                                    <td class="whitespace-nowrap px-6 py-4">
-                                        {{ $post->title }}
-                                    </td>
-                                    <td class="whitespace-nowrap px-6 py-4">
-                                        {{ Str::words($post->content, 5, '...') }}
-                                    </td>
-                                    <td class="whitespace-nowrap px-6 py-4">
-                                        {{ Str::wordCount($post->content) }}
-                                    </td>
-                                    <td class="whitespace-nowrap px-6 py-4 space-x-2 flex items-center justify-end text-right">
-                                        @can('update', $post)
-                                            <a href="{{ route('posts.edit', $post) }}"
+                        <div class="inline-block min-w-full space-y-6 py-2 sm:px-6 lg:px-8">
+                            <div class="overflow-hidden">
+                                <table class="min-w-full w-full text-left text-sm font-light">
+                                <thead
+                                    class="border-b bg-white font-medium dark:border-neutral-500 dark:bg-neutral-600">
+                                    <tr>
+                                    <th scope="col" class="px-6 py-4">#</th>
+                                    <th scope="col" class="px-6 py-4">Author</th>
+                                    <th scope="col" class="px-6 py-4">Title</th>
+                                    <th scope="col" class="px-6 py-4">Excerpt</th>
+                                    <th scope="col" class="px-6 py-4"># words</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($posts as $post)
+                                    <tr class="border-b {{ $loop->odd ? 'bg-white' : 'bg-neutral-100' }} dark:border-neutral-500 dark:bg-neutral-700">
+                                        <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                            {{ $post->id }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            {{ $post->user->name }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            {{ $post->title }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            {{ Str::words($post->content, 5, '...') }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            {{ Str::wordCount($post->content) }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4 space-x-2 flex items-center justify-end text-right">
+                                            @can('update', $post)
+                                                <a href="{{ route('posts.edit', $post) }}"
+                                                    class="text-xs text-blue-500 hover:underline transition"
+                                                >
+                                                    Edit
+                                                </a>
+                                            @endcan
+
+                                            <a href="{{ route('posts.show', $post) }}"
                                                 class="text-xs text-blue-500 hover:underline transition"
                                             >
-                                                Edit
+                                                Show
                                             </a>
-                                        @endcan
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                                </table>
+                            </div>
 
-                                        <a href="{{ route('posts.show', $post) }}"
-                                            class="text-xs text-blue-500 hover:underline transition"
-                                        >
-                                            Show
-                                        </a>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                            </table>
-                        </div>
+                            {{ $posts->links() }}
                         </div>
                     </div>
                 </div>
